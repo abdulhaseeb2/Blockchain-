5f2a4e1829606156da8e84e286aea7675802baa4
Abdul Haseeb <34100036+tigerdrifter@users.noreply.github.com>
2019-09-21 12:50:19 +0500

    Add files via upload

---
+++ i160296_SecA_A1.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+)
+
+type Block struct {
+	previousBlock *Block
+	hashValue     string
+	transaction   string
+}
+
+func hashBlock(block string) string {
+
+	// Create a new HMAC by defining the hash type and the key (as byte array)
+	h := hmac.New(sha256.New, []byte("secret"))
+
+	// Write Data to it
+	h.Write([]byte(block))
+
+	// Get result and encode as hexadecimal string
+	sha := hex.EncodeToString(h.Sum(nil))
+
+	return sha
+
+}
+
+func InsertBlock(transaction string, chainHead *Block) *Block {
+
+	var newBlock *Block = new(Block)
+
+	if chainHead == nil {
+
+		newBlock.transaction = transaction
+		newBlock.previousBlock = chainHead
+		newBlock.hashValue = ""
+		println("Genesis Block Added")
+	} else {
+
+		newBlock.transaction = transaction
+		newBlock.previousBlock = chainHead
+		newBlock.hashValue = hashBlock(chainHead.transaction + chainHead.hashValue)
+		println("New Block Added")
+	}
+
+	return newBlock
+}
+
+func VerifyChain(chainHead *Block) string {
+
+	if chainHead.previousBlock == nil { //genesis Node
+
+		return hashBlock(chainHead.transaction)
+
+	} else {
+		blockHash := VerifyChain(chainHead.previousBlock)
+
+		if blockHash == chainHead.hashValue {
+			println("Hash Matches")
+
+		} else {
+			println("Hash Does Not Match")
+
+		}
+
+		return hashBlock(chainHead.transaction + chainHead.hashValue)
+
+	}
+}
+
+func ChangeBlock(oldTrans string, newTrans string, chainHead *Block) {
+
+	if chainHead.previousBlock == nil { //Genesis Block
+
+		if chainHead.transaction == oldTrans { //If required block is found
+
+			chainHead.transaction = newTrans
+			println("Block Changed")
+		}
+
+	} else { //recursivly iterate to the required block
+
+		ChangeBlock(oldTrans, newTrans, chainHead.previousBlock)
+
+		if chainHead.transaction == oldTrans { //If required block is found
+
+			chainHead.transaction = newTrans
+			println("Block Changed")
+		}
+	}
+}
+
+func ListBlocks(chainHead *Block) {
+
+	if chainHead.previousBlock == nil { //genesis Node
+
+		println("Transaction: " + chainHead.transaction)
+		println("Genesis Block.\n")
+
+	} else {
+		println("Transaction " + chainHead.transaction)
+		println("Hash Value: " + chainHead.hashValue)
+
+		ListBlocks(chainHead.previousBlock)
+	}
+}
